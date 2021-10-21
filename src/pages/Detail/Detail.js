7e36e93139a66225b3fddc27bca595a68e432a6a
ilyas-bij <bijedigunilyas@gmail.com>
2021-10-21 15:28:51 +0100

    show data

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -7,8 +7,11 @@ import '../Scss/Detail.scss'
 
 function CoineDetail() {
   
-    const [coin, setcoin] = useState([])
-
+    const [Coin, setcoin] = useState([])
+    const [coinD, setcoinD] = useState([]);
+    const [Desc, setDesc] = useState('');
+    const [Mcap, setMcap] = useState('');
+ 
     var coinName = useParams()
     
     useEffect(() => {
@@ -18,29 +21,37 @@ function CoineDetail() {
           )
           .then(res => {
             setcoin(res.data);
-            console.log(res.data);
+            setcoinD(res.data);
+            setMcap(res.data.market_data)
+
+            console.log('new',res.data);
+
+           setDesc(res.data.description.en.replace(/<[^>]+>/g, ''))
+
           })
           .catch(error => console.log(error));
+         
           
       }, []);
      
+      const Show =({coin,Desc,Mcap})=>{
+        console.log(coin.length);
       
-      
-
-    return (
-        
-        <div>
-        <div style={{height:"70PX",backgroundColor:'#000'}}></div>
-        
-            <h1 >{coin.id}</h1>
+        if (coin.length !== 0) {
+            return(
+             <div>
+               <h1 >{coin.id}</h1>
             <section className='TopAria'>
               <div style={{display:'flex'}}> 
                 <div className='cards'>
-                  <h2>marcet cap</h2>
-                  <p>123.1203$</p>
+                  <h2>Market Cap</h2>
+                  <p>$ {Mcap &&Mcap.market_cap.usd.toLocaleString()}</p>
                   <p>2.12%</p>
                  </div>
-                <div className='cards'> </div>
+                <div className='cards'> 
+                <p>{coin.id} Price ({coin &&coin.symbol.toUpperCase()})</p>
+                <h2>{}</h2>
+                </div>
               </div>
               <div style={{display:'flex'}}> 
                 <div className='cards'> </div>
@@ -50,12 +61,60 @@ function CoineDetail() {
             </section>
 
             <section>
-              <h2>{coin.symbol} Price Live Data</h2>
-              
+              <h2>{coin && coin.symbol.toUpperCase()} Price Live Data</h2>
+              <p>{Desc}</p>
               
             </section>
+             </div>
+            )
+        }else{
+          return(
+            <div>
+          <h2>loading ...</h2>
+        </div>
+          )
+        }
+        
+        
+      }
+      
+    return (
+        
+        <div>
+        <div style={{height:"70PX",backgroundColor:'#000'}}></div>
+        <Show coin={Coin} Desc={Desc} Mcap={Mcap}/>
+       
         </div>
     )
 }
 
 export default CoineDetail
+
+/*
+<h1 >{coin.id}</h1>
+            <section className='TopAria'>
+              <div style={{display:'flex'}}> 
+                <div className='cards'>
+                  <h2>Market Cap</h2>
+                  <p>$ {Mcap &&Mcap.market_cap.usd.toLocaleString()}</p>
+                  <p>2.12%</p>
+                 </div>
+                <div className='cards'> 
+                <p>{coin.id} Price ({coin &&coin.symbol})</p>
+                <h2>{}</h2>
+                </div>
+              </div>
+              <div style={{display:'flex'}}> 
+                <div className='cards'> </div>
+                <div className='cards'> </div>
+              </div>
+           
+            </section>
+
+            <section>
+              <h2>{coin && coin.symbol.toUpperCase()} Price Live Data</h2>
+              <p>{Desc}</p>
+              
+            </section>
+
+      */
