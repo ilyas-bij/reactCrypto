755c9f1d51ecea033f4acca5d6042880606fd66b
ilyas-bij <bijedigunilyas@gmail.com>
2021-10-22 19:32:56 +0100

    detail css

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -11,6 +11,7 @@ function CoineDetail() {
     const [coinD, setcoinD] = useState([]);
     const [Desc, setDesc] = useState('');
     const [Mcap, setMcap] = useState('');
+    const [CoinP, setCoinP] = useState([]);
  
     var coinName = useParams()
     
@@ -23,8 +24,8 @@ function CoineDetail() {
             setcoin(res.data);
             setcoinD(res.data);
             setMcap(res.data.market_data)
-
-            console.log('new',res.data);
+            setCoinP(res.data.tickers.find(i=> i.target === 'USD'?i:null))
+         //  console.log('new',res.data);
 
            setDesc(res.data.description.en.replace(/<[^>]+>/g, ''))
 
@@ -33,34 +34,62 @@ function CoineDetail() {
          
           
       }, []);
+
+      //var CVP = CoinP.find(i=> i.target === 'USD'?i:null)
      
-      const Show =({coin,Desc,Mcap})=>{
-        console.log(coin.length);
-      
+     
+      const Show =({coin,Desc,Mcap,CVP})=>{
+       
+        const [per, setper] = useState(Mcap.market_cap_change_percentage_24h);
+     
         if (coin.length !== 0) {
             return(
              <div>
-               <h1 >{coin.id}</h1>
+               <h1 >{coin.id.toUpperCase()}</h1>
             <section className='TopAria'>
               <div style={{display:'flex'}}> 
+              <div className='cards'> 
+                <p>{coin.id} Price ({coin &&coin.symbol.toUpperCase()})</p>
+                <h2>${Mcap.current_price && Mcap.current_price.usd.toLocaleString()}</h2>
+                <div>
+                   <p>Low:$ <span>{Mcap.low_24h&& Mcap.low_24h.usd.toLocaleString()}</span></p>
+                   
+                    <p>High:$ <span>{Mcap.high_24h && Mcap.high_24h.usd.toLocaleString()}</span></p>
+                    
+                </div>
+                </div>
                 <div className='cards'>
                   <h2>Market Cap</h2>
                   <p>$ {Mcap &&Mcap.market_cap.usd.toLocaleString()}</p>
-                  <p>2.12%</p>
+                  <ul>
+                  {per && per < 0 ? (
+                            <li className='coin-percent red'>{per && per.toFixed(2)}%</li>
+                        ) : (
+                            <li className='coin-percent green'>{per && per.toFixed(2)}%</li>
+                        )} 
+                  </ul>
                  </div>
-                <div className='cards'> 
-                <p>{coin.id} Price ({coin &&coin.symbol.toUpperCase()})</p>
-                <h2>{}</h2>
-                </div>
+
+             
               </div>
               <div style={{display:'flex'}}> 
-                <div className='cards'> </div>
+                <div className='cards'>
+                <h2>Volume 24h</h2>
+                  <p>$ {Mcap &&Mcap.total_volume.usd.toLocaleString()}</p>
+                  <ul>
+                  {per && per < 0 ? (
+                            <li className='coin-percent red'>{per && per.toFixed(2)}%</li>
+                        ) : (
+                            <li className='coin-percent green'>{per && per.toFixed(2)}%</li>
+                        )} 
+                  </ul>
+                 </div>
                 <div className='cards'> </div>
               </div>
            
             </section>
 
-            <section>
+            <section className='coinDesc'>
               <h2>{coin && coin.symbol.toUpperCase()} Price Live Data</h2>
               <p>{Desc}</p>
               
@@ -82,7 +111,7 @@ function CoineDetail() {
         
         <div>
         <div style={{height:"70PX",backgroundColor:'#000'}}></div>
-        <Show coin={Coin} Desc={Desc} Mcap={Mcap}/>
+        <Show coin={Coin} Desc={Desc} Mcap={Mcap} CVP={CoinP}/>
        
         </div>
     )
